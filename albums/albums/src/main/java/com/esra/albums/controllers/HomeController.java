a125579b87700f9689e9625f97c00fadba547f86
Esra Yazar <esrayzr@gmail.com>
2021-12-28 13:16:11 -0500

    bugs fixed

--- albums/albums/src/main/java/com/esra/albums/controllers/HomeController.java
+++ albums/albums/src/main/java/com/esra/albums/controllers/HomeController.java
@@ -1,5 +1,24 @@
 package com.esra.albums.controllers;
 
-public class HomeController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import com.esra.albums.services.AlbumService;
+
+@Controller
+public class HomeController {
+	
+	@Autowired
+	private AlbumService aService;
+
+
+	@GetMapping("/")
+	public String index(Model viewModel) {
+	viewModel.addAttribute( "allAlbums", this.aService.getAllAlbums());
+	return "index.jsp";
+	}
+	
+	
 }
