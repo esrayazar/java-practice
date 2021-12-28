09c26ec3829ea36b12da7915a90c647e547d2eb1
Esra Yazar <esrayzr@gmail.com>
2021-12-28 13:13:11 -0500

    Home controller created

--- albums/albums/src/main/java/com/esra/albums/controllers/HomeController.java
+++ albums/albums/src/main/java/com/esra/albums/controllers/HomeController.java
@@ -1,38 +1,5 @@
 package com.esra.albums.controllers;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.esra.albums.models.Album;
-import com.esra.albums.services.AlbumService;
-
-@RestController
 public class HomeController {
-	@Autowired
-	private AlbumService aService;
-	
-	@GetMapping("/")
-	public List<Album> index(){
-		return this.aService.getAllAlbums();
-	}
-	
-	@GetMapping("/{id}")
-	public Album getOne(@PathVariable("id") Long id) {
-		return this.aService.getOneAlbum(id);
-	}
-	@PostMapping("/create")
-	public Album create (Album album) {
-		return this.aService.createAlbum(album);
-	}
-	@DeleteMapping("/delete/{id}")
-	public String delete(@PathVariable("id") Long id) {
-		return this.aService.deletealbum(id);
-	}
-	
+
 }
