3b2ddcedd6bac5bf178762ec338091ed951ae204
Esra Yazar <esrayzr@gmail.com>
2022-01-22 15:25:33 -0500

    User servicr created

--- albums/albums/src/main/java/com/esra/albums/services/UserService.java
+++ albums/albums/src/main/java/com/esra/albums/services/UserService.java
@@ -1,5 +1,22 @@
 package com.esra.albums.services;
 
-public class UserService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.esra.albums.models.User;
+import com.esra.albums.repositories.UserRepository;
+
+@Service
+public class UserService {
+	@Autowired
+	private UserRepository uRepo;
+	
+	public List<User> getAllUSers(){
+		return this.uRepo.findAll();
+	}
+	public User getOneUser(Long id) {
+		return this.uRepo.findById(id).orElse(null);
+	}
 }
