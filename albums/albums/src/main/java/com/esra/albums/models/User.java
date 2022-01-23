d2f07600a7f8200ba88ee316414f772024f24eb4
Esra Yazar <esrayzr@gmail.com>
2022-01-22 22:14:01 -0500

    bugs are fixed

--- albums/albums/src/main/java/com/esra/albums/models/User.java
+++ albums/albums/src/main/java/com/esra/albums/models/User.java
@@ -11,6 +11,10 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+import javax.persistence.Transient;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name="users")
@@ -18,8 +22,20 @@ public class User {
 	@Id
 	@GeneratedValue(strategy= GenerationType.IDENTITY)
 	private Long id;
+	@NotBlank
+	@Size(max=15)
 	private String firstName;
+	@NotBlank
+	@Size(max=30)
 	private String lastName;
+	@Email
+	@NotBlank
+	private String email;
+	private String password;
+	@Transient
+	private String confirmPassword; 
+	
+	
 	
 	@ManyToMany(fetch= FetchType.LAZY)
 			@JoinTable(
@@ -55,6 +71,24 @@ public class User {
 	}
 	public void setAlbumsliked(List<Album> albumsliked) {
 		this.albumsliked = albumsliked;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getConfirmPassword() {
+		return confirmPassword;
+	}
+	public void setConfirmPassword(String confirmPassword) {
+		this.confirmPassword = confirmPassword;
 	}
 	
 	
