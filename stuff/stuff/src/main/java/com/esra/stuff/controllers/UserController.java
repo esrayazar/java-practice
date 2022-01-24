ca727705d17d056b4bb8ae0f15fe245dafc2eb11
Esra Yazar <esrayzr@gmail.com>
2022-01-24 12:16:54 -0500

    user controller created

--- stuff/stuff/src/main/java/com/esra/stuff/controllers/UserController.java
+++ stuff/stuff/src/main/java/com/esra/stuff/controllers/UserController.java
@@ -9,15 +9,20 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.esra.stuff.models.User;
 import com.esra.stuff.services.UserService;
+import com.esra.stuff.validators.UserValidator;
 
 
 @Controller
 public class UserController {
 	@Autowired
 	private UserService userService;
+	@Autowired
+	private UserValidator validator;
 	
 	@GetMapping("/")
 	public String login(@ModelAttribute("user") User user) {
@@ -25,16 +30,31 @@ public class UserController {
 	}
 	
 	@PostMapping("/register")
-	public String register(@Valid @ModelAttribute("user") User user, BindingResult result, HttpSession session)}
+	public String register(@Valid @ModelAttribute("user") User user, BindingResult result, HttpSession session) {
 		validator.validate(user,result);
-	if(result.hasErrors()) {
-		return "landing.jsp";
+		if(result.hasErrors()) {
+			return "landing.jsp";
+		}
+		User newUser = this.userService.registerUser(user);
+		session.setAttribute("user__id", newUser.getId());
+		return "redirect:/thought/dashboard";
 	}
-	User newUser = this.uService.registerUser(user);
-	session.setAttribute("user__id", newUser.getId());
-	return "redirect:/dashboard";
-}
+	@PostMapping("/login")
+	public String login(HttpSession session, @RequestParam("lemail") String email, @RequestParam("lpassword") String password, RedirectAttributes redirectAttr) {
+		if(!this.userService.authenticateUser(email, password)) {
+			redirectAttr.addFlashAttribute("loginError", "Invalid Credentials");
+			return "redirect:/";
+		}
+		User userToBeLoggedIn= this.userService.getUserByEmail(email);
+		session.setAttribute("user__id",userToBeLoggedIn.getId());
+		return "redirect:/dashboard";
+	}
 	
-
+	@GetMapping("/logout")
+	public String logout(HttpSession session) {
+		session.invalidate();
+		return "redirect:/";
+	}
+	
 
 }
