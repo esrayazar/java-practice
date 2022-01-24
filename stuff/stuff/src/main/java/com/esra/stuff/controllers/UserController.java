2ddf2f7c9a89646f63670cfed62c8be44b121ed8
Esra Yazar <esrayzr@gmail.com>
2022-01-24 12:08:15 -0500

    user validator created

--- stuff/stuff/src/main/java/com/esra/stuff/controllers/UserController.java
+++ stuff/stuff/src/main/java/com/esra/stuff/controllers/UserController.java
@@ -1,9 +1,14 @@
 package com.esra.stuff.controllers;
 
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.esra.stuff.models.User;
 import com.esra.stuff.services.UserService;
@@ -18,8 +23,18 @@ public class UserController {
 	public String login(@ModelAttribute("user") User user) {
 		return "landing.jsp";
 	}
-//	@PostMapping("/register")
-//	public String register(@Valid @ModelAttribute("user") User user, BindingResult result)
+	
+	@PostMapping("/register")
+	public String register(@Valid @ModelAttribute("user") User user, BindingResult result, HttpSession session)}
+		validator.validate(user,result);
+	if(result.hasErrors()) {
+		return "landing.jsp";
+	}
+	User newUser = this.uService.registerUser(user);
+	session.setAttribute("user__id", newUser.getId());
+	return "redirect:/dashboard";
+}
+	
 
 
 }
--- stuff/stuff/src/main/java/com/esra/stuff/validators/UserValidator.java
+++ stuff/stuff/src/main/java/com/esra/stuff/validators/UserValidator.java
@@ -1,5 +1,34 @@
 package com.esra.stuff.validators;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+
+import com.esra.stuff.models.User;
+import com.esra.stuff.repositories.UserRepository;
+
+@Component
 public class UserValidator {
+	@Autowired
+	private UserRepository uRepo;
+	
+	public boolean supports(Class<?> clazz) {
+		return User.class.equals(clazz);
+	}
+	
+	public void validate(Object target, Errors errors) {
+		User user =(User) target;
+		if(!user.getPassword().equals(user.getConfirmPassword())) {
+			errors.rejectValue("password", "Match", "Password do not match!!!!!!!!");
+		}
+		if (this.uRepo.existsByEmail(user.getEmail())) {
+			errors.rejectValue("email", "Unique", "Email has already taken");
+		}
+		if (user.getFirstName().equals("Esra")) {
+			errors.rejectValue("firstName", "noMattAllowed", "Esra s  are unable to register at this time");
+		}
+		
+	}
 
 }
+
