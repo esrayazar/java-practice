91264f891926d6bf3db0df32b09fc84c90f383b2
Esra Yazar <esrayzr@gmail.com>
2022-01-26 15:12:20 -0500

    edit page created

--- musicapp/musicapp/src/main/java/com/esra/musicapp/controllers/HomeController.java
+++ musicapp/musicapp/src/main/java/com/esra/musicapp/controllers/HomeController.java
@@ -13,6 +13,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -31,13 +32,7 @@ public class HomeController {
 	@Autowired
 	private AlbumService albumService;
 	
-//	@GetMapping("/dashboard")
-//	public String albums(Model model, HttpSession session, @ModelAttribute("album") Album album) {
-//		User user = this.userService.findOneUser((Long) session.getAttribute("user__id"));
-//		model.addAttribute("user", user);
-//		model.addAttribute("allAlbums", this.albumService.allAlbums());
-//		return "dashboard.jsp";
-//	}
+
 	@GetMapping("/dashboard")
 	public String albums (Model model, HttpSession session) {
 		if(session.getAttribute("user__id") != null) {
@@ -72,40 +67,40 @@ public class HomeController {
 		this.albumService.create(album);
 		return "redirect:/albums/dashboard";
 	}
-//	//Get one album details 
-//	@GetMapping("/albums/project/{id}")
-//	public String project(@PathVariable("id")Long albumId, Model model,HttpSession session) {
-//		Album album = albumService.getOneAlbum(albumId);
-//		model.addAttribute("album", album);
-//		model.addAttribute("userLoggedIn", (Long)session.getAttribute("userId"));
-//	    return "album.jsp";
-//		
-//	}
-//	//Delete
-//	@PostMapping("/albums/delete/{id}")
-//	private String deleteAlbum(@PathVariable("id")Long albumId,HttpSession session) {
-//		albumService.deleteAlbum(albumId);
-//		return "redirect:/projects";
-//	}
-//	
-//	//Edit
-//	@GetMapping("albums/edit/{id}")
-//	public String edit(@PathVariable("id") Long id,@ModelAttribute("editedAlbum") Album album, Model model, HttpSession session) {
-//		Album editProject = albumService.getOneAlbum(id);
-//		model.addAttribute("editProject", editProject);
-//		return "edit.jsp";
-//	}
-//	
-//	//Update
-//	@PostMapping("/albums/update/{id}")
-//	public String update(@PathVariable("id") Long id,@Valid @ModelAttribute("editedAlbum") Album album, BindingResult result, HttpSession session, Model model) {
-//		if (result.hasErrors()) {
-//			model.addAttribute("album",this.albumService.getOneAlbum(id));
-//			return "edit.jsp";
-//		}
-//		this.albumService.updateAlbum(id,album.getAlbumName(), album.getDescription());
-//		return "redirect:/albums";
-//	}
+	//Get one album details 
+	@GetMapping("/album/{id}")
+	public String project(@PathVariable("id")Long albumId, Model model,HttpSession session) {
+		Album album = albumService.getOneAlbum(albumId);
+		model.addAttribute("album", album);
+		model.addAttribute("userLoggedIn", (Long)session.getAttribute("userId"));
+	    return "album.jsp";
+		
+	}
+	//Delete
+	@PostMapping("/delete/{id}")
+	private String deleteAlbum(@PathVariable("id")Long albumId,HttpSession session) {
+		albumService.deleteAlbum(albumId);
+		return "redirect:/projects";
+	}
+	
+	//Edit
+	@GetMapping("albums/edit/{id}")
+	public String edit(@PathVariable("id") Long id,@ModelAttribute("editedAlbum") Album album, Model model, HttpSession session) {
+		Album editAlbum = albumService.getOneAlbum(id);
+		model.addAttribute("editProject", editAlbum);
+		return "edit.jsp";
+	}
+	
+	//Update
+	@PostMapping("/albums/update/{id}")
+	public String update(@PathVariable("id") Long id,@Valid @ModelAttribute("editedAlbum") Album album, BindingResult result, HttpSession session, Model model) {
+		if (result.hasErrors()) {
+			model.addAttribute("album",this.albumService.getOneAlbum(id));
+			return "edit.jsp";
+		}
+		this.albumService.updateAlbum(id,album.getAlbumName(), album.getDescription());
+		return "redirect:/albums";
+	}
 //	@GetMapping("/{id}/like")
 //	public String like(HttpSession session, @PathVariable("id") Long id) {
 //		User user = this.userService.findOneUser((Long) session.getAttribute("user__id"));
